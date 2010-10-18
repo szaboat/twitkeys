3fd16274a44b56910145073db97edab819f96c36
Attila Szabo <attila.szabo@gmail.com>
2010-10-18 22:16:59 +0200

    - open link in background tab with o and in new tab with O - ignore links from twitter

--- keys.js
+++ keys.js
@@ -1,12 +1,30 @@
 $(function(){
   $('body').bind('keydown',function(e){ 
-    // observe the O key
-    if(e.which==79){
-      var item = $('.hovered-stream-item');
-      item.css({'background':'yellow'});
-      var url = $('.hovered-stream-item .tweet-text a').attr('href');
-      if(url)
-	window.open(url);
-    }
+		
+		var url;
+		var item = $('.hovered-stream-item');
+		var links = $('.hovered-stream-item .tweet-text a');
+
+		// check links function
+		if (links.length == 1){
+			if (links.attr('href').indexOf('twitter') == -1) {
+				url = links.attr('href')
+			}
+		} else {
+			links.each(function() {
+				if(this.href.indexOf('twitter') == -1){
+					url = this.href;
+				}
+			});
+		}
+
+		if( url && e.which==79 && e.shiftKey ) { 
+				window.open(url);
+				item.css({'background':'yellow'});
+    } else if ( url && e.which == 79 ) {
+				var msg = {"url": url};
+				chrome.extension.connect().postMessage(msg);
+				item.css({'background':'yellow'});
+		}
   });
 });
